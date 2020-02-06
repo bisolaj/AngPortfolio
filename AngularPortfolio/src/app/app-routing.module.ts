91c7fd6df5ff2551312eb8c0b79b067524911c2a
Code <code@COCTL-41288.local>
2020-02-06 15:34:07 -0800

    Second Commit

--- AngularPortfolio/src/app/app-routing.module.ts
+++ AngularPortfolio/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+import { ResumeComponent } from './resume/resume.component';
+
+const routes: Routes = [
+  {path: 'aboutme', component: AboutmeComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'resume', component: ResumeComponent},
+  {
+    path: '',
+    redirectTo: '/aboutme',
+    pathMatch: 'full'
+  }
 
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularPortfolio/src/app/app.module.ts
+++ AngularPortfolio/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    ProjectsComponent,
+    AboutmeComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
